f8624e73064563ac05086955ede4a5c331001d9d
Seb Carss <seb.carss@gmail.com>
2018-04-18 02:31:27 +0100

    Commit test site properly to GitHub

---
+++ src/pages/page-3.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "gatsby-link"
+
+export default () => (
+    <div>
+        <h1>Looks like you made it to the third page</h1>
+        <Link to="/">back home</Link>
+    </div>
+);
