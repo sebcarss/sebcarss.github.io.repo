7047b10bb82512181ffb6a82c1d8bda9903c1baf
Seb Carss <seb.carss@gmail.com>
2018-04-18 03:17:02 +0100

    Cleaned up site back to basics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,6 @@
 import React from "react";
-import Link from "gatsby-link";
 
 export default () =>
-    <div style={{ color: `tomato` }}>
-        <h1>Hello Seb!</h1>
-        <p>What a world!</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
-        <br />
-        <div>
-            <Link to="/page-2/">Page 2</Link>
-        </div>
-        <div>
-            <Link to="/page-3/">Page 3</Link>
-        </div>
-        <div>
-            <Link to="/counter/">Counter</Link>
-        </div>
+    <div>
+        <p>...coming soon</p>
     </div>
