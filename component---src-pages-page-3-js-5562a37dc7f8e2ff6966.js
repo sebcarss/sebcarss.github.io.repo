1446a7de4b235c7fb20ce620831ff5f98e6755a5
Seb Carss <seb.carss@gmail.com>
2018-04-18 01:05:23 +0100

    Testing Gatsby

---
+++ component---src-pages-page-3-js-5562a37dc7f8e2ff6966.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81ce6852476c],{196:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var a=u(5),n=l(a),o=u(98),d=l(o);t.default=function(){return n.default.createElement("div",null,n.default.createElement("h1",null,"Looks like you made it to the third page"),n.default.createElement(d.default,{to:"/"},"back home"))},e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-page-3-js-5562a37dc7f8e2ff6966.js.map
