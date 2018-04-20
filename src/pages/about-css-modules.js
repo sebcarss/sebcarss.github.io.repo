fac032a036cd20ceded59bceb8dac73a75dded3d
Seb Carss <seb.carss@gmail.com>
2018-04-20 14:56:49 +0100

    Add container component

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Container from "../../src/components/container";
+
+export default () => (
+    <Container>
+        <h1>About CSS Modules</h1>
+        <p>CSS Modules are cool</p>
+    </Container>
+);
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,8 @@
 import Typography from "typography";
-// import bootstrapTheme from "typography-theme-bootstrap";
+import bootstrapTheme from "typography-theme-bootstrap";
 // import lawtonTheme from "typography-theme-lawton";
-import lincolnTheme from "typography-theme-lincoln";
+// import lincolnTheme from "typography-theme-lincoln";
 
-const typography = new Typography(lincolnTheme);
+const typography = new Typography(bootstrapTheme);
 
 export default typography;
