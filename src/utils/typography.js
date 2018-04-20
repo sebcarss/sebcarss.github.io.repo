a9f6665cad12b322760fb4946c169ad5f6517e87
Seb Carss <seb.carss@gmail.com>
2018-04-20 14:33:52 +0100

    Add the Lincoln theme for my actual site

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,8 @@
 import Typography from "typography";
 // import bootstrapTheme from "typography-theme-bootstrap";
-import lawtonTheme from "typography-theme-lawton";
+// import lawtonTheme from "typography-theme-lawton";
+import lincolnTheme from "typography-theme-lincoln";
 
-const typography = new Typography(lawtonTheme);
+const typography = new Typography(lincolnTheme);
 
 export default typography;
