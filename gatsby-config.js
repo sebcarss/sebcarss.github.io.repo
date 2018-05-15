df6bacfd7da8ab6981cdaa92bc3b09d40ba3bf49
Seb Carss <seb.carss@gmail.com>
2018-05-15 21:45:14 +0100

    Add ability to add images to markdown
    This update allows you to add a path to a local image with
    ![title](image-path) and it will automatically blur large images as required
    in the same way medium and Facebook do, as well as get the correct srcsets to
    make the site performant
    
    commit cad4867118c0f6280b5de9be0edb7ab5fff6d90b
    Author: Seb Carss <seb.carss@gmail.com>
    Date:   Tue May 15 21:42:59 2018 +0100
    
        Get images working from any folder
    
    commit 63d3b77ef3c79f9e3377b76ae7e07b0239cec6e4
    Author: Seb Carss <seb.carss@gmail.com>
    Date:   Tue May 15 21:22:02 2018 +0100
    
        Update gatsby-cli to 1.1.52
    
    commit c2d7182d1c9e147032b638920620306a9abc30ee
    Author: Seb Carss <seb.carss@gmail.com>
    Date:   Fri May 11 18:57:00 2018 +0100
    
        Add gatsby-remark-images and gatsby-plugin-sharp dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,22 @@ module.exports = {
                 path: `${__dirname}/src/`,
             },
         },
-        `gatsby-transformer-remark`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            // See options for more detais https://www.gatsbyjs.org/packages/gatsby-remark-images/
+                            maxWidth: 650,
+                            linkImagesToOriginal: true,
+                            showCaptions: false,
+                        }
+                    }
+                ]
+            }
+        }
     ],
 };
