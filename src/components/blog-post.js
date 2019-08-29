14367550f1930894f2ea6ddc8337e97d8c28a9e2
Seb Carss <seb.carss@gmail.com>
2019-08-29 07:25:35 +0100

    Add a featured image to the blog posts

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Layout from './layout';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 export default ({ data }) => {
     const post = data.markdownRemark;
+    let featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
+    
     return (
         <Layout>
             <div>
                 <h1 style={{ color: `#585858`, marginBottom: `-0.10em` }}>{post.frontmatter.title}</h1>
                 <div style={{ color: `teal`, marginBottom: `1em`, borderBottom: `solid 1px #585858` }}>Part of the {post.frontmatter.series} series</div>
+                <Img fluid={featuredImgFluid} />
                 <div style={{ color: `#585858` }} dangerouslySetInnerHTML={{ __html: post.html }} />
             </div>
         </Layout>
@@ -22,6 +27,13 @@ export const query = graphql`
         series
         title
         date
+        featuredImage {
+            childImageSharp {
+                fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
       }
     }
   }
